1c454781809cb020b10f4d06c2741b418e90dcc6
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2026-05-24 14:50:23 -0300

    configurando tela gerenciamento de processos

--- core/views.py
+++ core/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 @login_required
-def home(request):
-    return render(request, 'home.html')
+def gerenciamento_processos(request):
+    return render(request, 'gerenciamento_processos.html')
 
 # Rota teste -> apenas para visualizar o arquivo base.html para desenvolvimento
 def base(request):
--- setup/urls.py
+++ setup/urls.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from core.forms import CustomLoginForm
-from core.views import home, base
+from core.views import gerenciamento_processos, base
 
 urlpatterns = [
     # Rota admin
     path('admin/', admin.site.urls),
     # Rota raiz (home)
-    path('', home, name='home'),
+    path('', gerenciamento_processos, name='gerenciamento_processos'),
     # Rotas de autenticação
     path('login/', auth_views.LoginView.as_view(
         template_name='login.html',
         authentication_form=CustomLoginForm # Adicione esta linha
     ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # renderizar template Base para testes e desenvolvimento
     path('base/', base, name='base'),
 ]
