99e496502f4c4ec0cfa3540ec94f55f1ebd2c267
Henirque Cardoso <henrique.cardoso@poupex.com.br>
2026-05-26 17:50:28 -0300

    criaçao das models do app notificacoes

--- notificacoes/models.py
+++ notificacoes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from processos.models import Processo
 # Create your models here.
+
+class Notificacao(models.Model):
+    processo = models.ForeignKey(
+        Processo,
+        on_delete=models.CASCADE,
+        related_name='notificacoes'
+    )
+    titulo = models.CharField(max_length=255)
+    mensagem = models.TextField()
+    categoria = models.CharField(max_length=100)
+    data_geracao = models.DateTimeField(auto_now_add=True)
+    is_enviada_email = models.BooleanField(default=False)
+    is_lida = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Notificação"
+        verbose_name_plural = "Notificações"
+        ordering = ['-data_geracao']
+    
+    def __str__(self):
+        return self.titulo
