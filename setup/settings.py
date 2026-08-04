6203033608656116164c655e96b6c5fd9a885dbd
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2026-08-04 02:42:40 -0300

    correção settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -89,11 +89,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST', default='supera_db'),
-            'PORT': config('DB_PORT', default='5432'),
+            'NAME': config('POSTGRES_DB'),
+            'USER': config('POSTGRES_USER'),
+            'PASSWORD': config('POSTGRES_PASSWORD'),
+            'HOST': config('POSTGRES_HOST', default='supera_db'),
+            'PORT': config('POSTGRES_PORT', default='5432'),
         }
     }
 
